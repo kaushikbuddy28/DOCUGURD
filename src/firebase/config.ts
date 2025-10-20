d75bed15e04dab2602fadb3157665d40a104d227
Kaushik Chaturvedi <kaushikchaturvedi3535@gmail.com>
2025-10-20 10:47:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2597157348-c1c90",
+  "appId": "1:337193111287:web:57942e33cb213dfc1b8df2",
+  "apiKey": "AIzaSyBAuhPQu4BCv0qzZTUJUCG0KFg_jTo96ok",
+  "authDomain": "studio-2597157348-c1c90.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "337193111287"
+};
